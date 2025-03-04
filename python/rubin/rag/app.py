4d947295d3656dfa04545d4a024c3c17eac225cb
cyab05 <cyab05@uw.edu>
2025-03-04 09:32:41 -0800

    Fix streamlit app and init

--- python/rubin/rag/app.py
+++ python/rubin/rag/app.py
@@ -1,9 +1,38 @@
-import streamlit as st
-from dotenv import load_dotenv
-from langchain_community.chat_message_histories import \
-    StreamlitChatMessageHistory
+#
+# This file is part of rubin_rag.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""Set up the Streamlit interface for the chatbot, configuring the
+retriever, QA chain, session state, UI elements, and handling user of
+interactions.
+"""
+
+from pathlib import Path
+
+import streamlit as st
 from chatbot import configure_retriever, create_qa_chain, handle_user_input
+from dotenv import load_dotenv
+from langchain_community.chat_message_histories import (
+    StreamlitChatMessageHistory,
+)
 from layout import setup_header_and_footer, setup_landing_page, setup_sidebar
 
 # Load environment variables from .env file
@@ -18,7 +47,8 @@ st.set_page_config(
 st.logo("../../../static/logo.png")
 
 # Load the CSS file
-with open("../../../static/style.css") as css:
+file_path = Path("../../../static/style.css")
+with Path.open(file_path) as css:
     st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
 
 # Set up the session state
