4d02f33144df8ae9e19f516c67e00279569ea9f7
saamievincken <saamievincken@protonmail.com>
2025-05-21 10:05:12 +0200

    Update app.py

--- python/euclid/rag/chatbot.py
+++ python/euclid/rag/chatbot.py
@@ -49,7 +49,7 @@ from langchain_core.vectorstores.base import VectorStoreRetriever
 from langchain_ollama import OllamaLLM
 
 from euclid import STATIC_DIR
-
+from .FAISS_vectorstore.VectorstoreEmbedder import E5MpsEmbedder
 from .streamlit_callback import get_streamlit_cb
 
 
